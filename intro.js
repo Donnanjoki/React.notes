0e50e980b3324f1fb54ee9928de74356f5a147a8
Donna Njoki <njerinjoki2@gmail.com>
2023-07-26 12:42:11 +0300

    react notes

--- intro.js
+++ intro.js
@@ -3,4 +3,39 @@
 /* Chapter one:
 Built: By Facebook, 2013
 Built to cater to large-scale, data-driven websites.
+
+***Obstacles and Roadblocks with using react***
+1. React is a library
+The react library is small hence only used to cater to a part of the job. In addition to this 
+new tools keep emerging hence leaving the developer with a plethora of libraries that may make it impossible to keep up.
+
+2. New ECMAScript Syntax
+
+3. Popularity of Functional Javascript
+Though Javascript is not necessarily a functional language, functional techniques can be utilized.
+React emphasizes functional programming over object-oriented programming.
+
+4. Javascript Tooling Fatigue
+You might love React, but now you also need to be a webpack
+expert, handling code splitting, compression, testing, and on and on.
+
+*** React Developer Tools ***
+ 
+1. react-detector 
+ Which lets you know which sites are using react and which are not.
+
+2. React Developer Tools 
+This is a plugin that can extend the functionality of the browser’s developer tools.
+It creates a new tab in the developer tools where you can view React elements.
+
+*/
+
+/* Chapter 2: Emerging Javascript
+*** Declaring variables in ES6  ***
+Prior to ES6 the only way to declare variables was with var keyword. But with the recent updates
+you can declare variables with;
+
+a] const 
+
+
 */
